5e4aaa13dec3ca50e74a47bf81f19e726ba313ee
GBDO <dogibum6@gmail.com>
2022-04-10 23:52:25 +0900

    Update 3장.sql

--- 3장.sql
+++ 3장.sql
@@ -57,3 +57,13 @@
     ## '2016-01-30'::date AS dt
     ## , '2016-01-30 12:00:00'::timestamp AS stamp 
 ;
+
+    SELECT
+    stamp
+    ## PostgreSQL, BigQuery EXTRACT 함수 사용
+    ,EXTRACT(YEAR FROM stamp) AS year
+    ,EXTRACT(MONTH FROM stamp) AS month
+    ,EXTRACT(DAY FROM stamp) AS day
+    ,EXTRACT(HOUR FROM stamp) AS hour
+    FROM
+    (SELECT CAST('2016-01-30 12:00:00' AS timestamp) AS stamp) AS t;
